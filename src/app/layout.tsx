5fc79a7ac9a7d516eef2e6ba9801125c65aab5d3
Mehran Bolhasani <bolhasani.mehran@gmail.com>
2025-10-25 16:01:50 +0200

    Add vercel analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import "./globals.css";
 import { ThemeProvider } from "next-themes";
 
+import { Analytics } from "@vercel/analytics/next"
+
 
 export const metadata: Metadata = {
   title: {
@@ -76,6 +78,7 @@ export default function RootLayout({
         >
           {children}
         </ThemeProvider>
+        <Analytics />
       </body>
     </html>
   );
